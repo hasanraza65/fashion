cee259dad89a8bebf6f90e89b069632491a828e8
Hassan Raza <ranahasanraza24@gmail.com>
2022-08-27 15:38:47 +0500

    create category id foriegn key

---
+++ database/migrations/2022_08_27_102146_addcolto_products.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddcoltoProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('category_id')->nullable()->after('name')->constrained('product_categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
